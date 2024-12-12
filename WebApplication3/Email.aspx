6e7f8580940fe1cc64f25759a24ba853c008ce84
tousifansari829 <50771870+tousifansari829@users.noreply.github.com>
2024-12-12 22:47:40 +0530

    Add project files.

---
+++ WebApplication3/Email.aspx
@@ -0,0 +1,31 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Email.aspx.cs" Inherits="WebApplication3.Email" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            To<asp:TextBox ID="TextBox1" runat="server"></asp:TextBox>
+            <br />
+            <br />
+            Subject
+            <asp:TextBox ID="TextBox2" runat="server"></asp:TextBox>
+            <br />
+            <br />
+            Body<asp:TextBox ID="TextBox3" runat="server"></asp:TextBox>
+            <br />
+            <br />
+            Attachment<asp:FileUpload ID="FileUpload1" runat="server" />
+            <br />
+            <br />
+            <asp:Button ID="Button1" runat="server" OnClick="Button1_Click" Text="Send" />
+            <br />
+            <br />
+        </div>
+    </form>
+</body>
+</html>
---
+++ WebApplication3/Email.aspx.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Mail;
+using System.IO;
+using System.Net;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebApplication3
+{
+    public partial class Email : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            MailMessage mailMessage = new MailMessage();
+            mailMessage.From = new MailAddress("tousifansari829@gmail.com");
+            mailMessage.To.Add(TextBox1.Text);
+            mailMessage.Subject = TextBox2.Text;
+            mailMessage.Body = TextBox3.Text;   
+
+            if (FileUpload1.HasFile)
+            {
+                string fileName = Path.GetFileName(FileUpload1.FileName);
+                string filePath = Path.Combine(Server.MapPath("Attachment"),fileName);
+                FileUpload1.SaveAs(filePath);
+                mailMessage.Attachments.Add(new Attachment(filePath));
+            }
+
+            SmtpClient smtp = new SmtpClient("smtp.gmail.com");
+            smtp.Port = 587;
+            smtp.EnableSsl = true;
+            smtp.Credentials = new NetworkCredential("tousifansari829@gmail.com", "hlwjuhqxmrmxzlyv");
+            smtp.Send(mailMessage); 
+            Response.Write("<script>alert('Mail Sent Successfully')</script>");
+
+
+        }
+    }
+}
